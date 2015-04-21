46f93c281c3622917a7380e20cd4eaca6db75b51
Kovács József <smith@sztaki.hu>
2015-04-21 15:43:38 +0200

    Parameters updated.

--- jobflow_submit.py
+++ jobflow_submit.py
@@ -2,21 +2,21 @@ import requests
 import yaml
 import os,sys,stat
 
-url = "http://192.168.153.107:5000/jobflow"
-filetosend = "config-job.yaml"
+url = "http://192.168.153.107:6000/jobflow"
+filetosend = os.path.join(sys.prefix,"etc","config-job.yaml")
 
 def readconfig(pathtoconfig):
     with open(pathtoconfig, 'r') as f:
         return yaml.load(f)
 
-sysconfpath = os.path.join('config-sys.yaml')
-confsys = dict(readconfig(sysconfpath))
+sysconfpath = os.path.join(sys.prefix,'etc','jobflow-config-sys.yaml')
+confsys = readconfig(sysconfpath)
 
 def perform_sending_content(url,content):
     r = requests.post(url, data=yaml.dump(content))
 
 def submit_a_job():
-    perform_sending_content(url,dict(readconfig(filetosend)))
+    perform_sending_content(url,readconfig(filetosend))
 
 
 submit_a_job()
