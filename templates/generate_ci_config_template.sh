38c228e3ad30d2b41a38a17053d63f37b02d876a
Kovács József <smith@sztaki.hu>
2015-05-21 12:32:21 +0200

    Small fix

--- templates/generate_ci_config_template.sh
+++ templates/generate_ci_config_template.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+SCRIPTDIR=".."
 TARGET="template_jobflow_ci_config_base.yaml"
 BASESYSTMPL="template_jobflow_ci_config_frame_sys.yaml"
 BASEAPPTMPL="template_jobflow_ci_config_frame_app.yaml"
-python create_jobflow_template_frames.py --base $BASESYSTMPL --scriptdir .. > $TARGET
+python $SCRIPTDIR/create_jobflow_template_frames.py --base $BASESYSTMPL --scriptdir $SCRIPTDIR > $TARGET
 cat $BASEAPPTMPL >> $TARGET
 echo "Input sys template: $BASESYSTMPL"
 echo "Input app template: $BASEAPPTMPL"
