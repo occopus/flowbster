a61df72bed5673a3cee78d62756af370e6522045
Kovács József <smith@sztaki.hu>
2015-05-21 14:21:22 +0200

    Removing unnecessary code

--- examples/single_node_cat/1_render_ci_config_with_app.sh
+++ examples/single_node_cat/1_render_ci_config_with_app.sh
@@ -5,15 +5,6 @@ TARGET="template_jobflow_ci_config_app.yaml"
 TMPL="$JFDIR/templates/template_jobflow_ci_config_base.yaml"
 VALUES="values_jobflow_app.yaml"
 
-echo $*
-
-if [ $# -gt 0 ]; then
-    VALUES=$1
-fi
-if [ $# -gt 1 ]; then
-    TARGET=$2
-fi
-
 python $JFDIR/render_jobflow_templates.py --template $TMPL --values $VALUES > $TARGET
 echo "Input template: $TMPL"
 echo "Input values  : $VALUES"
--- examples/single_node_cat/2_render_ci_config_with_cons.sh
+++ examples/single_node_cat/2_render_ci_config_with_cons.sh
@@ -5,15 +5,6 @@ TARGET="template_jobflow_ci_config_con.yaml"
 TMPL="template_jobflow_ci_config_app.yaml"
 VALUES="values_jobflow_con.yaml"
 
-echo $*
-
-if [ $# -gt 0 ]; then
-    VALUES=$1
-fi
-if [ $# -gt 1 ]; then
-    TARGET=$2
-fi
-
 python $JFDIR/render_jobflow_templates.py --template $TMPL --values $VALUES > $TARGET
 echo "Input template: $TMPL"
 echo "Input values  : $VALUES"
