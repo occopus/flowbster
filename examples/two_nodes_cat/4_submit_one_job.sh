c5f72c21bc4d8f64bf9b2ad59d58c8304335774f
Kovács József <smith@sztaki.hu>
2015-05-22 10:41:37 +0200

    Small corrections

--- examples/two_nodes_cat/4_submit_one_job.sh
+++ examples/two_nodes_cat/4_submit_one_job.sh
@@ -2,7 +2,7 @@
 
 JFDIR="../.."
 JOBFILE="jobflow-config-job-with-content.yaml"
-TARGETIP="192.168.152.242"
+TARGETIP="192.168.152.249"
 echo "Jobfile  : $JOBFILE"
 echo "Target ip: $TARGETIP"
 python $JFDIR/jobflow_submit.py $JOBFILE http://$TARGETIP:5000/jobflow
