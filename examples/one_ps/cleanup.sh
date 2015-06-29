101fcce46be4c721ab11a8ee79349692df67621d
Jozsef Kovacs <jozsef.kovacs@sztaki.mta.hu>
2015-06-29 16:54:01 +0200

    cleaning up collector files

--- examples/one_ps/cleanup.sh
+++ examples/one_ps/cleanup.sh
@@ -3,9 +3,11 @@
 rm -f jobflow-receiver.log
 rm -f jobflow-executor.log
 rm -f jobflow-forwarder.log
+rm -f jobflow-collector.log
 rm -f jobflow-receiver.pid
 rm -f jobflow-executor.pid
 rm -f jobflow-forwarder.pid
+rm -f jobflow-collector.pid
 
 
 
