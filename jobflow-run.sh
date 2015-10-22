28b4d027cb1f7c32a75f6c1aba2052f4e0e7c2b9
Zoltán Farkas <farkas.zoltan@sztaki.mta.hu>
2015-10-22 10:48:03 +0200

    Removed 'jobflow' user from context script. Place scripts in /usr/bin.

--- jobflow-run.sh
+++ jobflow-run.sh
@@ -3,42 +3,42 @@
 mkdir -p /var/log/jobflow
 
 if [ ! -e /var/run/jobflow-receiver.pid ]; then
-    cd /home/jobflow
+    cd /usr/bin
     python jobflow_receiver.py &
     echo $! > /var/run/jobflow-receiver.pid
 else
     pid=`cat /var/run/jobflow-receiver.pid`
     if [ ! -e /proc/$pid -a /proc/$pid/exe ]; then
         echo "Cannot find jobflow-receiver. Restarting..."
-        cd /home/jobflow
+        cd /usr/bin
         python jobflow_receiver.py &
         echo $! > /var/run/jobflow-receiver.pid
     fi
 fi
 
 if [ ! -e /var/run/jobflow-executor.pid ]; then
-    cd /home/jobflow
+    cd /usr/bin
     python jobflow_executor.py &
     echo $! > /var/run/jobflow-executor.pid
 else
     pid=`cat /var/run/jobflow-executor.pid`
     if [ ! -e /proc/$pid -a /proc/$pid/exe ]; then
         echo "Cannot find jobflow-executor. Restarting..."
-        cd /home/jobflow
+        cd /usr/bin
         python jobflow_executor.py &
         echo $! > /var/run/jobflow-executor.pid
     fi
 fi
 
 if [ ! -e /var/run/jobflow-forwarder.pid ]; then
-    cd /home/jobflow
+    cd /usr/bin
     python jobflow_forwarder.py &
     echo $! > /var/run/jobflow-forwarder.pid
 else
     pid=`cat /var/run/jobflow-forwarder.pid`
     if [ ! -e /proc/$pid -a /proc/$pid/exe ]; then
         echo "Cannot find jobflow-forwarder. Restarting..."
-        cd /home/jobflow
+        cd /usr/bin
         python jobflow_forwarder.py &
         echo $! > /var/run/jobflow-forwarder.pid
     fi
