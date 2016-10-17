b3f85e05bb80f1d157abe5ed75a7b990bed8ca0c
Jozsef Kovacs <smith@sztaki.hu>
2016-10-17 16:48:19 +0200

    Small fix

--- examples/str/bin/do_statistics.sh
+++ examples/str/bin/do_statistics.sh
@@ -2,5 +2,5 @@
 
 echo "Number of characters: `cat text | wc -m`" > stat
 echo "Number of words: `cat text | wc -w`" >> stat
-echo "Number of line: `cat text | wc -l`" >> stat
+echo "Number of lines: `cat text | wc -l`" >> stat
 
