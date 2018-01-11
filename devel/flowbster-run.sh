6718f429c90cbf7087465105e111523102ab46eb
Zoltán FARKAS <zfarkas@sztaki.hu>
2018-01-11 08:33:34 +0000

    prepared for Docker

--- devel/flowbster-run.sh
+++ devel/flowbster-run.sh
@@ -2,6 +2,13 @@
 
 mkdir -p /var/log/flowbster
 
+if [ ! -e /etc/flowbster-config-sys.yaml ]; then
+    [ -z "$FLOWBSTER_SYS_CFG" ] && echo "Variable FLOWBSTER_SYS_CFG is not set!" && exit 1
+    [ -z "$FLOWBSTER_APP_CFG" ] && echo "Variable FLOWBSTER_APP_CFG is not set!" && exit 1
+    echo "$FLOWBSTER_SYS_CFG" | base64 -d > /etc/flowbster-config-sys.yaml
+    echo "$FLOWBSTER_APP_CFG" | base64 -d > /etc/flowbster-config-app.yaml
+fi
+
 if [ ! -e /var/run/flowbster-receiver.pid ]; then
     cd /usr/bin
     python flowbster_receiver.py &
@@ -44,8 +51,8 @@ else
     fi
 fi
 
+[ ! -f /.dockerenv ] && exit 0
 
-
-
-
-
+while true; do
+    sleep 60
+done
