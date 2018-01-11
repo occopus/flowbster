a46188ed288b68f2b419df584443d0841cfca0c5
Zoltán FARKAS <zfarkas@sztaki.hu>
2018-01-11 09:34:08 +0000

    make necessary process checking method nice for both vm- and docker- based execution

--- devel/flowbster-run.sh
+++ devel/flowbster-run.sh
@@ -11,52 +11,56 @@ if [ ! -e /etc/flowbster-config-sys.yaml ]; then
     echo "$FLOWBSTER_APP_CFG" | base64 -d > /etc/flowbster-config-app.yaml
 fi
 
-if [ ! -e /var/run/flowbster-receiver.pid ]; then
-    cd /usr/bin
-    python flowbster_receiver.py &
-    echo $! > /var/run/flowbster-receiver.pid
-else
-    pid=`cat /var/run/flowbster-receiver.pid`
-    if [ ! -e /proc/$pid -a /proc/$pid/exe ]; then
-        echo "Cannot find flowbster-receiver. Restarting..."
+# Check if necessary flowbster processes are alive
+function check_processes {
+    if [ ! -e /var/run/flowbster-receiver.pid ]; then
         cd /usr/bin
         python flowbster_receiver.py &
         echo $! > /var/run/flowbster-receiver.pid
+    else
+        pid=`cat /var/run/flowbster-receiver.pid`
+        if [ ! -e /proc/$pid -a /proc/$pid/exe ]; then
+            echo "Cannot find flowbster-receiver. Restarting..."
+            cd /usr/bin
+            python flowbster_receiver.py &
+            echo $! > /var/run/flowbster-receiver.pid
+        fi
     fi
-fi
 
-if [ ! -e /var/run/flowbster-executor.pid ]; then
-    cd /usr/bin
-    python flowbster_executor.py &
-    echo $! > /var/run/flowbster-executor.pid
-else
-    pid=`cat /var/run/flowbster-executor.pid`
-    if [ ! -e /proc/$pid -a /proc/$pid/exe ]; then
-        echo "Cannot find flowbster-executor. Restarting..."
+    if [ ! -e /var/run/flowbster-executor.pid ]; then
         cd /usr/bin
         python flowbster_executor.py &
         echo $! > /var/run/flowbster-executor.pid
+    else
+        pid=`cat /var/run/flowbster-executor.pid`
+        if [ ! -e /proc/$pid -a /proc/$pid/exe ]; then
+            echo "Cannot find flowbster-executor. Restarting..."
+            cd /usr/bin
+            python flowbster_executor.py &
+            echo $! > /var/run/flowbster-executor.pid
+        fi
     fi
-fi
 
-if [ ! -e /var/run/flowbster-forwarder.pid ]; then
-    cd /usr/bin
-    python flowbster_forwarder.py &
-    echo $! > /var/run/flowbster-forwarder.pid
-else
-    pid=`cat /var/run/flowbster-forwarder.pid`
-    if [ ! -e /proc/$pid -a /proc/$pid/exe ]; then
-        echo "Cannot find flowbster-forwarder. Restarting..."
+    if [ ! -e /var/run/flowbster-forwarder.pid ]; then
         cd /usr/bin
         python flowbster_forwarder.py &
         echo $! > /var/run/flowbster-forwarder.pid
+    else
+        pid=`cat /var/run/flowbster-forwarder.pid`
+        if [ ! -e /proc/$pid -a /proc/$pid/exe ]; then
+            echo "Cannot find flowbster-forwarder. Restarting..."
+            cd /usr/bin
+            python flowbster_forwarder.py &
+            echo $! > /var/run/flowbster-forwarder.pid
+        fi
     fi
-fi
+}
 
 # Check if running inside a Docker container, and exit if not
-[ ! -f /.dockerenv ] && exit 0
+[ ! -f /.dockerenv ] && check_processes && exit 0
 
 # Keep the script running inside Docker
 while true; do
+    check_processes
     sleep 60
 done
